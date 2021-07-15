2b112eb191204c05e76b9fb2a15126ac5a40e633
rogerio carvalho <rogerio.carvalho.silva15@gmail.com>
2021-07-15 18:58:18 -0300

    TEST PAULO

--- main_test_configs.py
+++ main_test_configs.py
@@ -1,4 +1,4 @@
-
+# Importing the libraries
 import sys
 
 import numpy as np
@@ -6,12 +6,28 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from keras.losses import mean_absolute_percentage_error
 from keras.models import Sequential
+from keras.layers import Dropout
+from sklearn.utils import shuffle
 from keras.layers import Dense, Bidirectional
-from keras.layers import LSTM
+from keras.layers.recurrent import LSTM, GRU
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import GridSearchCV
+
+import time
+import tensorflow as tf
+from sklearn.metrics import confusion_matrix, classification_report, roc_auc_score
+from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score
+from keras import backend as K
+
+
+
+
 # Importing the libraries
 from math import sqrt
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from tensorflow.python.ops.metrics_impl import root_mean_squared_error
+#import mysql.connector as connection
 
 
 def score_lstm(estimator, X, y, scoring, cv):
@@ -25,17 +41,6 @@ def score_lstm(estimator, X, y, scoring, cv):
     estimator.fit(X_train, y_train)
     y_pred = estimator.predict(X_test)
 
-    # scorer = {'neg_mean_absolute_error': 'neg_mean_absolute_error',
-    #           'neg_mean_squared_error': 'neg_mean_squared_error', 'r2': 'r2',
-    #           'mean_absolute_percentage_error': make_scorer(mean_absolute_percentage_error,
-    #                                                         greater_is_better=False),
-    #           'root_mean_squared_error': make_scorer(root_mean_squared_error, greater_is_better=False)}
-
-    print('X_test: ', X_test)
-    print('y:', y.shape)
-    print('y_train:', y_train.shape)
-    print('ytest: ', y_test.shape)
-    print('y_pred:', y_pred.shape)
     scoring['neg_mean_absolute_error'] = mean_absolute_error(y_test, y_pred)
     scoring['neg_mean_squared_error'] = mean_squared_error(y_test, y_pred)
     scoring['r2'] = r2_score(y_test, y_pred)
@@ -54,6 +59,8 @@ def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
     Returns:
         Pandas DataFrame of series framed for supervised learning.
     """
+    print("series_to_supervised")
+
     n_vars = 1 if type(data) is list else data.shape[1]
     df = pd.DataFrame(data)
     cols, names = list(), list()
@@ -61,6 +68,7 @@ def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
     for i in range(n_in, 0, -1):
         cols.append(df.shift(i))
         names += [('var%d(t-%d)' % (j + 1, i)) for j in range(n_vars)]
+      
     # forecast sequence (t, t+1, ... t+n)
     for i in range(0, n_out):
         cols.append(df.shift(-i))
@@ -69,6 +77,7 @@ def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
         else:
             names += [('var%d(t+%d)' % (j + 1, i)) for j in range(n_vars)]
     # put it all together
+    
     agg = pd.concat(cols, axis=1)
     agg.columns = names
     # drop rows with NaN values
@@ -76,17 +85,14 @@ def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
         agg.dropna(inplace=True)
     return agg
 
+
 def main(DATASET, WINDOW_SIZE, VERSIONS_AHEAD):
-
-
-
 
     METRIC_KEYS_SDK4ED = ['bugs', 'duplicated_blocks', 'code_smells',
                           # 'comment_lines', 'ncloc', 'uncovered_lines', 'vulnerabilities', 'complexity',
                           'sqale_index', 'reliability_remediation_effort', 'security_remediation_effort']
     results = {}
     results['_info'] = {'metrics': METRIC_KEYS_SDK4ED, 'window_size': WINDOW_SIZE, 'versions_ahead': VERSIONS_AHEAD}
-
 
     def create_regressor(reg_type, X, Y, project, versions_ahead):
         # Splitting the dataset into the Training set and Test set
@@ -96,6 +102,23 @@ def main(DATASET, WINDOW_SIZE, VERSIONS_AHEAD):
         from sklearn.pipeline import Pipeline
         from sklearn.preprocessing import StandardScaler
         scaler = StandardScaler()
+        #custom parameters for lstm
+        batch_size = [2,5,10, 15, 20, 25, 30]
+        epochs = [100,500,1000,1500]
+        optimizer = ['adam']
+        learn_rate = [0.001, 0.01, 0.1, 0.2, 0.3]
+        momentum = [0.0, 0.2, 0.4, 0.6, 0.8, 0.9]
+        init_mode = ['uniform', 'lecun_uniform', 'normal', 'zero', 'glorot_normal', 'glorot_uniform', 'he_normal', 'he_uniform']
+        activation = ['softmax', 'softplus', 'softsign', 'relu', 'tanh', 'sigmoid', 'hard_sigmoid', 'linear']
+        weight_constraint = [1, 2, 3, 4, 5]
+        dropout_rate = [0.0,0.05, 0.1, 0.2, 0.3]
+        neurons = [10, 50, 100, 150, 200, 250, 300]
+
+        #param_grid = dict(optimizer = optimizer)
+        param_grid = {
+                        'regressor__optimizer': optimizer,
+                     }
+
         # define base model
         def baseline_model():
             # create model
@@ -106,25 +129,17 @@ def main(DATASET, WINDOW_SIZE, VERSIONS_AHEAD):
             model.compile(loss='mean_squared_error', optimizer='adam')
             return model
 
-        def lstm_model():
+        def lstm_model(optimizer='adam'):
             # LSTM layer expects inputs to have shape of (batch_size, timesteps, input_dim).
             # In keras you need to pass (timesteps, input_dim) for input_shape argument.
 
-            neurons = [200]
-
-            #model = Sequential()
-            #model.add(LSTM(250, input_shape=(10, 1), kernel_initializer='normal', activation='relu'))
-            #model.add(Dense(1, kernel_initializer='normal'))
-            # Compile model
-            #model.compile(loss='mean_squared_error', optimizer='adam')
-            #return model
             model = Sequential()
-            model.add(LSTM(150, input_shape=(10, 1),return_sequences=True, kernel_initializer='normal', activation='relu'))
-            model.add(LSTM(50,activation='relu', return_sequences=True, input_shape=(10, 1)))
-            model.add(LSTM(10,activation='relu', return_sequences=False, input_shape=(10, 1)))
+            model.add(LSTM(200, input_shape=(9, 1), kernel_initializer='normal', activation='relu'))
             model.add(Dense(1, kernel_initializer='normal'))
-            model.compile(loss='mean_squared_error', optimizer='adam')
+            model.compile(loss='mean_squared_error', optimizer=optimizer)
             return model
+            
+
         # Create the regressor model
         if reg_type == 'LinearRegression':
             # Fitting Multiple Linear Regression to the Training set
@@ -164,7 +179,7 @@ def main(DATASET, WINDOW_SIZE, VERSIONS_AHEAD):
         elif reg_type == 'ANN':
             # Fitting ANN to the dataset
             from keras.wrappers.scikit_learn import KerasRegressor
-            regressor = KerasRegressor(build_fn=baseline_model, epochs=1000, batch_size=5, verbose=True)
+            regressor = KerasRegressor(build_fn=baseline_model, epochs=1000, batch_size=5, verbose=False)
             pipeline = Pipeline([('scaler', scaler), ('regressor', regressor)])
         elif reg_type == 'LSTM':
             from keras.wrappers.scikit_learn import KerasRegressor
@@ -189,37 +204,35 @@ def main(DATASET, WINDOW_SIZE, VERSIONS_AHEAD):
                   'mean_absolute_percentage_error': make_scorer(mean_absolute_percentage_error,
                                                                 greater_is_better=False),
                   'root_mean_squared_error': make_scorer(root_mean_squared_error, greater_is_better=False)}
-        
+
         if reg_type == 'LSTM':
             # reshape from [samples, timesteps] into [samples, timesteps, features]
             n_features = 1
             X = X.reshape((X.shape[0], X.shape[1], n_features))
 
-        print(X.shape)
-        print(Y.shape)
-        # if reg_type == 'LSTM':
-        #     scores = score_lstm(estimator=pipeline, X=X, y=Y.ravel(), scoring=scorer, cv=tscv)
-        # else:
-        scores = cross_validate(estimator=pipeline, X=X, y=Y.ravel(), scoring=scorer, cv=tscv, return_train_score=False)
+        #scores = cross_validate(estimator=pipeline, X=X, y=Y.ravel(), scoring=scorer, cv=tscv, return_train_score=False)
+        search = GridSearchCV(estimator=pipeline, param_grid=param_grid, n_jobs=-1, scoring=scorer, refit=False, return_train_score=False)
+        search.fit(X=X, y=Y.ravel())
 
-        # Fill results dict object
-        for key, value in scores.items():
-            print(key, value)
+        print("Best parameter (CV score=%0.3f):")
+        print(search.best_params_)
+        search.scorer_.items
+        for key, value in  search.scorer_.items():
+           # print(key, value)
             scores[key] = value.tolist()
-        results[project][versions_ahead][reg_type] = scores
+        results[project][versions_ahead][reg_type] = search.scorer_
 
-    #     regressor.fit(X_train, Y_train)
-    #     results[project][versions_ahead][reg_type]['test_set_r2'] = regressor.score(X_test, Y_test)
-
-    # For every project in dataset
+   # For every project in dataset
     for project in DATASET:
         # Initialize results dict object
         results[project] = {}
         for versions_ahead in VERSIONS_AHEAD:
+            print("Version ahead " +  str(versions_ahead))
             # Initialize results dict object
             results[project][versions_ahead] = {}
 
             # Importing the dataset
+            #coloca 3 colunas em uma so e dpois dropa essas 3 colunas
             dataset = pd.read_csv(project + '.csv', sep=";", usecols=METRIC_KEYS_SDK4ED)
             dataset['total_principal'] = dataset['reliability_remediation_effort'] + dataset[
                 'security_remediation_effort'] + dataset['sqale_index']
@@ -228,26 +241,46 @@ def main(DATASET, WINDOW_SIZE, VERSIONS_AHEAD):
 
             # Adding time-shifted prior and future period
             data = series_to_supervised(dataset.values, n_in=WINDOW_SIZE)
-
             # Append dependend variable column with value equal to next version's total_principal
             data['forecasted_total_principal'] = data['var4(t)'].shift(-versions_ahead)
             data = data.drop(data.index[-versions_ahead:])
-
             # Remove total_cost from indpependent variables set
-            data = data.drop(columns=['var4(t-2)', 'var4(t-1)'])
+            #drop pra nao alienar o algoritmo (creio eu)
+            data = data.drop(columns=['var4(t-2)', 'var4(t-1)', 'var4(t)'])
 
+            #### ADAPTACAO MELO ESTRUTURA RECORRENTE ####
+
+            #para transformar na representação do melo é preciso splitar as linhas 
+            # em colunas baseado no tamanho da janela e no número de variaveis
+            timeseries_list = list()
+            timeseries_labels = list()
+            for i in range(0,data.shape[0]):
+                window = list()
+                for j in range(0,data.shape[1]-1,3):
+                     window.append(data.iloc[i,j:j+3 ].values.astype(np.float64))
+                timeseries_list.append(window)
+                timeseries_labels.append(data.iloc[i, data.shape[1]-1])
+            
+            timeseries_tensor = np.array(timeseries_list)
+
+            timeseries_tensor = timeseries_tensor.transpose((0,2,1))
+            
+            timeseries_labels = np.array(timeseries_labels).astype(np.float64)
+            timeseries_labels = timeseries_labels.reshape(-1, 1)
+
+            #### FIM ADAPTACAO MELO ESTRUTURA RECORRENTE ####
+            
             # Set X, Y
             X = data.iloc[:, data.columns != 'forecasted_total_principal'].values
             # Y = data.iloc[:, data.columns == 'forecasted_total_principal'].values
             Y = data.forecasted_total_principal.values
-            
-
-
-
-
-            # for reg_type in ['LinearRegression', 'LassoRegression', 'RidgeRegression', 'SGDRegression', 'SVR_rbf', 'SVR_linear', 'RandomForestRegressor']:
+            #for reg_type in ['LinearRegression', 'LassoRegression', 'RidgeRegression', 'SGDRegression', 'SVR_rbf', 'SVR_linear', 'RandomForestRegressor', 'LSTM']:
             for reg_type in ['LSTM']:
-                regressor = create_regressor(reg_type, X, Y, project, versions_ahead)
+                if(reg_type == 'LSTM'):
+                    regressor = create_regressor(reg_type, X, Y, project, versions_ahead)
+                    #regressor = create_regressor(reg_type, timeseries_tensor, timeseries_labels, project, versions_ahead)
+                else:
+                    regressor = create_regressor(reg_type, X, Y, project, versions_ahead)
 
     print_forecasting_errors(VERSIONS_AHEAD, reg_type, results, versions_ahead, DATASET)
 
@@ -275,16 +308,16 @@ def read_td_dataset(VERSIONS_AHEAD, WINDOW_SIZE):
     data = data.drop(columns=['var4(t-2)', 'var4(t-1)'])
     X = data.iloc[:, data.columns != 'forecasted_total_principal'].values
     Y = data.iloc[:, data.columns == 'forecasted_total_principal'].values
+   
     return X, Y, METRIC_KEYS_SDK4ED
 
-
-def print_forecasting_errors(VERSIONS_AHEAD, reg_type, results, versions_ahead,DATASET):
+def print_forecasting_errors(VERSIONS_AHEAD, reg_type, results, versions_ahead, DATASET):
     for project in DATASET:
         print('**************** %s ****************' % project)
         print(results[project][versions_ahead][reg_type])
-        for reg_type in ['LSTM']: #, 'LinearRegression', 'LassoRegression', 'RidgeRegression', 'SGDRegression', 'SVR_rbf',
-                         #'SVR_linear', 'RandomForestRegressor']:
-            print('================ %s ================' % reg_type)
+        #for reg_type in ['LinearRegression', 'LassoRegression', 'RidgeRegression', 'SGDRegression', 'SVR_rbf', 'SVR_linear', 'RandomForestRegressor', 'LSTM']:
+        for reg_type in ['LSTM']:
+            print('*************** %s **************' % reg_type)
             for versions_ahead in VERSIONS_AHEAD:
                 # Print scores
                 mae_mean = np.asarray(results[project][versions_ahead][reg_type]['test_neg_mean_absolute_error']).mean()
@@ -301,7 +334,9 @@ def print_forecasting_errors(VERSIONS_AHEAD, reg_type, results, versions_ahead,D
                 rmse_std = np.asarray(results[project][versions_ahead][reg_type]['test_root_mean_squared_error']).std()
                 # test_set_r2 = results[project][versions_ahead][reg_type]['test_set_r2']
 
-                print('%0.3f;%0.3f;%0.3f;%0.3f' % (abs(mae_mean), abs(rmse_mean), abs(mape_mean), r2_mean))
+                print('%0.3f,%0.3f,%0.3f,%0.3f' % (abs(mae_mean), abs(rmse_mean), abs(mape_mean), r2_mean))
+
+
 
 if __name__ == '__main__':
     # Selecting indicators that will be used as model variables
@@ -309,26 +344,28 @@ if __name__ == '__main__':
     # 'AMC', 'WMC', 'DIT', 'NOC', 'RFC', 'CBO', 'Ca', 'Ce', 'CBM', 'IC', 'LCOM', 'LCOM3', 'CAM', 'NPM', 'DAM', 'MOA']
     # 'Security Index', 'blocker_violations', 'critical_violations', 'major_violations', 'minor_violations', 'info_violations']
     DATASET = ['_benchmark_repository_measures',
-                'apache_groovy_measures',
-                'apache_incubator_dubbo_measures',
-                'apache_kafka_measures',
-                'apache_nifi_measures',
-                'apache_ofbiz_measures',
-                'apache_systemml_measures',
-                'commonsio_measures',
-                'company_projectA_measures',
-                'company_projectB_measures',
-                'google_guava_measures',
-                'igniterealtime_openfire_measures',
-                'java_websocket_measures',
-                'jenkinsci_jenkins_measures',
-                'spring-projects_spring-boot_measures',
-                'square_okhttp_measures',
-                'square_retrofit_measures',
-                'zxing_zxing_measures'
+               # 'apache_groovy_measures',
+               # 'apache_incubator_dubbo_measures',
+               # 'apache_kafka_measures',
+               # 'apache_nifi_measures',
+               # 'apache_ofbiz_measures',
+               # 'apache_systemml_measures',
+               # 'commonsio_measures',
+               # 'company_projectA_measures',
+               # 'company_projectB_measures',
+               # 'google_guava_measures',
+               # 'igniterealtime_openfire_measures',
+                #'java_websocket_measures',
+               # 'jenkinsci_jenkins_measures',
+               # 'spring-projects_spring-boot_measures',
+                #'square_okhttp_measures',
+                #'square_retrofit_measures',
+                #'zxing_zxing_measures'
                  ]
+                
 
     WINDOW_SIZE = 2  # choose based on error minimization for different forecasting horizons
-    VERSIONS_AHEAD = [1, 5, 10, 20, 40]
+    VERSIONS_AHEAD = [2, 5, 10, 20, 40]
 
     main(DATASET, WINDOW_SIZE, VERSIONS_AHEAD)
+
