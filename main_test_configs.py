12edec0ef68c607afe47020c55c55f8211392a1e
rogerio carvalho <rogerio.carvalho.silva15@gmail.com>
2021-07-23 16:03:46 -0300

    test two databases

--- main_test_configs.py
+++ main_test_configs.py
@@ -372,7 +372,7 @@ if __name__ == '__main__':
     DATASET = ['_benchmark_repository_measures',
                # 'apache_groovy_measures',
                # 'apache_incubator_dubbo_measures',
-               # 'apache_kafka_measures',
+                'apache_kafka_measures'
                # 'apache_nifi_measures',
                # 'apache_ofbiz_measures',
                # 'apache_systemml_measures',
